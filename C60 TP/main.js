459edbfd906a422877c24983fce72bef84583425
lusianox <bucher.luciano19@gmail.com>
2023-06-06 07:23:23 -0300

    Trabajo-final
    Hola <3

---
+++ C60 TP/main.js
@@ -0,0 +1,11 @@
+function registroWeb(){
+    // let usuario = document.getElementById("usuario").value;
+    // let password = document.getElementById("password").value;
+    alert('Registro exitoso!');
+}
+
+function enviarMensaje(){
+    // let usuario = document.getElementById("usuario").value;
+    // let password = document.getElementById("password").value;
+    alert('Mensaje enviado exitosamente');
+}
